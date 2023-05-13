6b38e9334d41b709fff4618e511a531034394057
Wojtek <pulchnymamut@gmail.com>
2023-05-13 15:20:41 +0200

    formatting

--- Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -20,11 +20,35 @@ def calculate(list):
 
     # Calculating mean, variance, standard deviation, max, min and sum using numpy
     calculations = {
-        'mean': [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix)],
-        'variance': [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix)],
-        'standard deviation': [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix)],
-        'max': [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix)],
-        'min': [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix)],
-        'sum': [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix)]
+        'mean': [
+            np.mean(matrix, axis=0).tolist(),
+            np.mean(matrix, axis=1).tolist(),
+            np.mean(matrix)
+        ],
+        'variance': [
+            np.var(matrix, axis=0).tolist(),
+            np.var(matrix, axis=1).tolist(),
+            np.var(matrix)
+        ],
+        'standard deviation': [
+            np.std(matrix, axis=0).tolist(),
+            np.std(matrix, axis=1).tolist(),
+            np.std(matrix)
+        ],
+        'max': [
+            np.max(matrix, axis=0).tolist(),
+            np.max(matrix, axis=1).tolist(),
+            np.max(matrix)
+        ],
+        'min': [
+            np.min(matrix, axis=0).tolist(),
+            np.min(matrix, axis=1).tolist(),
+            np.min(matrix)
+        ],
+        'sum': [
+            np.sum(matrix, axis=0).tolist(),
+            np.sum(matrix, axis=1).tolist(),
+            np.sum(matrix)
+        ]
     }
     return calculations
