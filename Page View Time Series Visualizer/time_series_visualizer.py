cd0abc368f2c856d18a3ac46aee7bd6dd4311a31
Wojtek <pulchnymamut@gmail.com>
2023-05-14 10:20:00 +0200

    lineplot working

--- Page View Time Series Visualizer/time_series_visualizer.py
+++ Page View Time Series Visualizer/time_series_visualizer.py
@@ -18,7 +18,8 @@ df['date'] = pd.to_datetime(df['date'])
 df.set_index('date')
 
 # Clean data
-df = df.loc[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
+df = df.loc[(df['value'] >= df['value'].quantile(0.025)) &
+            (df['value'] <= df['value'].quantile(0.975))]
 
 
 def draw_line_plot():
@@ -27,11 +28,12 @@ def draw_line_plot():
     ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
     ax.set_xlabel("Date")
     ax.set_ylabel("Page Views")
-    sns.lineplot(data=df, legend=False)
+    ax.plot(df.index, df['value'], color='red')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
     return fig
+
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
@@ -39,13 +41,10 @@ def draw_bar_plot():
 
     # Draw bar plot
 
-
-
-
-
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
@@ -55,10 +54,6 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
-
-
-
-
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
